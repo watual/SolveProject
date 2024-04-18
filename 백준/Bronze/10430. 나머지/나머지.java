2238effc91fd6152bcaf1aa1d5009ec67cca24d6
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-19 00:06:57 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import javax.security.sasl.SaslException;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+
+        System.out.println((a+b)%c);
+        System.out.println((a%c+b%c)%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
