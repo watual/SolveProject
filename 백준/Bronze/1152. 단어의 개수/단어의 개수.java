142384c135aa03173033901bceda677e4d264d79
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-19 00:16:03 +0900

    [Bronze II] Title: 단어의 개수, Time: 288 ms, Memory: 20420 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        
+        bw.write(st.countTokens() + "\n");
+        bw.close();
+    }
+}
