4d355dca3cc9025be1bd74db27067ee85f4dd5dd
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-19 00:05:54 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+
+        System.out.println(a-543);
+        s.close();
+
+    }
+}
