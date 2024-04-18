20099d13457921f7a6604f7e1edd288c238d585f
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-19 00:10:40 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 856 ms, Memory: 235312 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            bw.write(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+"\n");
+            //bw.flush();
+        }
+        bw.close();
+    }
+}
