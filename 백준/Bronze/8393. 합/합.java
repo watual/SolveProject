194012a29f04be59c22ab47990e7ea4ddc5e2a50
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-19 00:09:38 +0900

    [Bronze V] Title: 합, Time: 212 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        System.out.println((n + 1) * n / 2);
+
+        scan.close();
+    }
+}
