a20e713c37a410b946f749261b3feee8703ac683
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-19 00:05:45 +0900

    [Bronze V] Title: ??!, Time: 224 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String a = s.nextLine();
+
+        System.out.println(a+"??!");
+        s.close();
+
+    }
+}
