77e02feafd4e267cff1e8c4cdf99f777f15b8d10
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-19 00:07:10 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        long a = s.nextLong();
+        long b = s.nextLong();
+        long c = s.nextLong();
+
+        System.out.print(a+b+c);
+    }
+}
